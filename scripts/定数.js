4f59faa751db245f0133b6364f5f36410eff04da
towa5165 <nonwish2524@gmail.com>
2021-08-29 00:32:15 +0900

    追加漏れ

---
+++ scripts/定数.js
@@ -0,0 +1,11 @@
+/** Twitter取得 **/
+const SHEET_TWEET_ID = 'スプレッドシートID';
+//例
+const TWITTER_EXAMPLE = 'スクリーンネーム("TwitterJP" 等)';
+const WEBHOOK_EXAMPLE = 'discordのウェブフックURL';
+const SHEET_EXAMPLE_NAME = 'スプレッドシートのシート名';
+
+/** シート関係 **/
+// 保持MAX
+const MAX_NUM = 20;
+
